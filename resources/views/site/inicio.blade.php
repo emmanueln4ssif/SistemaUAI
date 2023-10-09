f0de596b9664178d0fc4a39783a457e68be5ce2e
Emmanuel Nassif <emmanuel.nassif@gmail.com>
2023-10-08 22:39:24 -0300

    initial project

---
+++ resources/views/site/inicio.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Sistema UAI</title>
+    </head>
+    <body>
+        <header>
+            <nav>
+                <ul>
+                    <li><a href={{ asset('/') }}>Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href={{ asset('login') }}>Login</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <h1>Welcome to My Landing Page</h1>
+            <p>Here's some information about my product or service.</p>
+        </main>
+        <footer>
+            <p>Contact us at info@mylandingpage.com</p>
+        </footer>
+    </body>
+</html>
